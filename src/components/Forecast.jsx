56683bf82a04ca9fc551d3a93cd0beba84d39f08
Raisul999 <raisulislamarnob0@gmail.com>
2023-08-14 22:30:28 +0600

    refactor

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,37 +1,30 @@
-import { iconUrlFromCode } from "../services/weatherService"
+import { iconUrlFromCode } from "../services/weatherService";
 
-const Forecast = ({ title, items }) => {
-        return (
-                <div>
-                        <div className="flex text-center items-center justify-center mt-6 sm:justify-start">
-                                <p className='text-white font-medium uppercase '>
-                                        {title}
-                                </p>
-                        </div>
-                        <hr className="my-2" />
-                        <div className="flex flex-grow items-center justify-around gap-3 text-white ">
-                                {
-                                        items.map((item) => (
-                                                <div className="flex flex-col items-center justify-center">
-                                                        <p className="font-light text-sm">
-                                                                {item.title}
-                                                        </p>
-                                                        <img
-                                                                src={iconUrlFromCode(item.icon)}
-                                                                alt=""
-                                                                className="w-12 my-1"
-                                                        />
-                                                        <p className="font-medium">
-                                                                {Math.floor(item.temp)}&deg;C
-                                                        </p>
+const Forecast = ({ title, items, units }) => {
+  return (
+    <div>
+      <div className="flex text-center items-center justify-center mt-6 sm:justify-start">
+        <p className="text-white font-medium uppercase ">{title}</p>
+      </div>
+      <hr className="my-2" />
+      <div className="flex flex-grow items-center justify-around gap-3 text-white ">
+        {items.map((item, i) => (
+          <div key={i} className="flex flex-col items-center justify-center">
+            <p className="font-light text-sm">{item.title}</p>
+            <img
+              src={iconUrlFromCode(item.icon)}
+              alt=""
+              className="w-12 my-1"
+            />
+            <p className="font-medium">
+              {Math.floor(item.temp)}
+              {units == "metric" ? <span>&deg;C</span> : <span>&deg;F</span>}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-                                                </div>
-                                        ))
-                                }
-
-                        </div>
-                </div>
-        )
-}
-
-export default Forecast 
+export default Forecast;
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,85 +1,83 @@
-import React, { useState } from 'react'
-import { UilSearch, UilLocationPoint } from '@iconscout/react-unicons'
+import React, { useState } from "react";
+import { UilSearch, UilLocationPoint } from "@iconscout/react-unicons";
 
 const Input = ({ setQuery, units, setUnits }) => {
+  const [city, setCity] = useState("");
 
-        const [city, setCity] = useState('')
+  const handleSearch = () => {
+    if (city !== "") {
+      setQuery({ q: city });
+    } else {
+      alert("Please enter a city name ");
+    }
+  };
 
-        const handleSearch = () => {
-                if (city !== '') {
-                        setQuery({ q: city })
-                } else {
-                        alert('Please enter a city name ')
-                }
-        }
+  const handleLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((pos) => {
+        let lat = pos.coords.latitude;
+        let lon = pos.coords.longitude;
 
-        const handleLocation = () => {
-                if (navigator.geolocation) {
-                        navigator.geolocation.getCurrentPosition((pos) => {
-                                let lat = pos.coords.latitude
-                                let lon = pos.coords.longitude
+        setQuery({
+          lat,
+          lon,
+        });
+      });
+    }
+  };
 
-                                setQuery({
-                                        lat,
-                                        lon
-                                })
-                        })
-                }
-        }
+  const handleUnitsChange = (e) => {
+    let selectedunit = e.target.name;
 
-        const handleUnitsChange = (e) => {
-                let selectedunit = e.target.name
+    console.log("selected unit", selectedunit);
 
-                if (units !== selectedunit) {
-                        setUnits(selectedunit)
-                }
-        }
-        return (
-                <div className='flex flex-col justify-center my-6'>
-                        <div className='flex justify-center items-center space-x-4'>
-                                <input
-                                        type="text"
-                                        className='text-md font-light p-2 max-auto shadow-xl focus:outline-none capitalize sm:w-full '
-                                        placeholder='Search city...'
-                                        value={city}
-                                        onChange={(e) => setCity(e.target.value)}
-                                />
-                                <button>
-                                        <UilSearch
-                                                size={25}
-                                                className='text-white cursor-pointer transition ease-out hover:scale-125'
-                                                onClick={handleSearch}
-                                        />
-                                </button>
-                                <button>
-                                        <UilLocationPoint
-                                                className='text-white cursor-pointer transition ease-out hover:scale-125'
-                                                onClick={handleLocation}
-                                        />
-                                </button>
+    if (units !== selectedunit) {
+      setUnits(selectedunit);
+    }
+  };
+  return (
+    <div className="flex flex-col justify-center my-6">
+      <div className="flex justify-center items-center space-x-4">
+        <input
+          type="text"
+          className="text-md font-light p-2 max-auto shadow-xl focus:outline-none capitalize sm:w-full "
+          placeholder="Search city..."
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button>
+          <UilSearch
+            size={25}
+            className="text-white cursor-pointer transition ease-out hover:scale-125"
+            onClick={handleSearch}
+          />
+        </button>
+        <button>
+          <UilLocationPoint
+            className="text-white cursor-pointer transition ease-out hover:scale-125"
+            onClick={handleLocation}
+          />
+        </button>
+      </div>
+      <div className="flex justify-center items-center mt-4">
+        <button
+          name="metric"
+          className="text-xl text-white font-light hover:scale-110 duration-200"
+          onClick={handleUnitsChange}
+        >
+          &deg;C
+        </button>
+        <p className="text-white px-2">|</p>
+        <button
+          name="imperial"
+          className="text-xl text-white font-light hover:scale-110 duration-200"
+          onClick={handleUnitsChange}
+        >
+          &deg;F
+        </button>
+      </div>
+    </div>
+  );
+};
 
-
-                        </div>
-                        <div className='flex justify-center items-center mt-4'>
-
-                                <button
-                                        name='metric'
-                                        className='text-xl text-white font-light hover:scale-110 duration-200'
-                                        onClick={handleUnitsChange}
-                                >
-                                        &deg;C
-                                </button>
-                                <p className='text-white px-2'>|</p>
-                                <button
-                                        name='imperial'
-                                        className='text-xl text-white font-light hover:scale-110 duration-200'
-                                        onClick={handleUnitsChange}
-                                >
-                                        &deg;F
-                                </button>
-                        </div>
-                </div>
-        )
-}
-
-export default Input
+export default Input;
